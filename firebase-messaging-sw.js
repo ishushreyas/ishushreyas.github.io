fa9984f3d08cdf21832f752d55b57ca9485f21f6
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2022-12-06 23:12:38 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -27,6 +27,5 @@ messaging.onBackgroundMessage((payload) => {
     icon: '/firebase-logo.png'
   };
 
-  self.registration.showNotification(notificationTitle,
-    notificationOptions);
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
