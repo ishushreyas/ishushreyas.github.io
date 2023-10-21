55c366dc485a50cc2dfebddd32bc544062b9883e
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2023-10-21 18:09:33 +0530

    Update script.js

--- project/foc/cloud/script.js
+++ project/foc/cloud/script.js
@@ -3,7 +3,7 @@ function changeCss () {
     var scrllText = document.querySelector("#scroll");
     var img3 = document.querySelector("#img3");
     var img5 = document.querySelector("#img5");
-    scrllText.textContent = this.scrollY;
+    //scrllText.textContent = this.scrollY;
     this.scrollY > 800 && this.scrollY < 2250 ? navElement.style.display = 'block' : navElement.style.display = 'none';
     this.scrollY > 700 && this.scrollY < 1200 ? img3.style.transform = 'translate(0px)' : img3.style.transform = 'translate(' + (this.scrollY - 600) +'px)';
     this.scrollY > 900 && this.scrollY < 1600 ? img5.style.transform = 'translate(0px)' : img5.style.transform = 'translate(-' + (this.scrollY - 600) +'px)';
