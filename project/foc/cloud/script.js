1d2d25e113b16b6a9d3d1e28ace9e25e489b0099
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2023-11-03 21:56:20 +0530

    Update script.js

--- project/foc/cloud/script.js
+++ project/foc/cloud/script.js
@@ -13,7 +13,7 @@ this.scrollY > img3.getBoundingClientRect().top ? img3.style.opacity = '1' : img
 this.scrollY > img5.getBoundingClientRect().top ? img5.style.opacity = '1' : img5.style.opacity = '0';
 this.scrollY > secElement.getBoundingClientRect().top ? document.body.style.backgroundColor = '#7ee787' : console.log("Not in 3");
 this.scrollY > lstElement.getBoundingClientRect().top ? document.body.style.backgroundColor = '#939aff' : console.log("Not in 5");
-document.querySelector("#img6").getBoundingClientRect().top ? document.body.style.backgroundColor = '#ffffff' : console.log("Not in up");
+document.querySelector("#box6").getBoundingClientRect().top ? document.body.style.backgroundColor = '#ffffff' : console.log("Not in up");
   }
   window.addEventListener("scroll", changeCss , false);
   
